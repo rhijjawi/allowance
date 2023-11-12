7738eedbc3dbe4400c7d838ec518dee436ebd0ff
Ramzi H <github@ramzihijjawi.me>
2023-11-12 13:56:09 +0100

    allow apple's dumb domain cert

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
  
 //https://clerk.com/docs/references/nextjs/auth-middleware
 export default authMiddleware({
-    publicRoutes: ['/', '/api(.*)', '/trpc(.*)', '/faq', '/debt/calculator', '/privacy-policy', '/terms-of-service'],
+    publicRoutes: ['/', '/api(.*)', '/trpc(.*)', '/faq', '/debt/calculator', '/privacy-policy', '/terms-of-service', '/.well-known(.*)'],
 });
  
 export const config = {
