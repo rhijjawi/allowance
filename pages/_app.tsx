47ae18c6c6074faa731b87c8d39421533437714a
Ramzi H <github@ramzihijjawi.me>
2023-11-14 16:04:02 +0100

    pizdyet kurwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,10 +28,25 @@ export default function MyApp({Component, pageProps }: {Component: any, pageProp
                             <title key="main">LogMoney.app | The better money app</title>
                             <meta property="og:title" content="LogMoney.app | The better money app" key="title" />
                             <meta property='og:description' content='Discover LogMoney.app - the smart choice for responsible financial oversight. Discover how students and spenders across the world keep themselves financially accountable.' key='description' />
-                            <meta property='og:image' content='https://logmoney.app/og/image.png' key='image' />
+                            <meta property='og:image' content='https://logmoney.app/og.png' key='image' />
                             <meta property='og:url' content='https://logmoney.app' key='url' />
                             <meta property='og:type' content='website' key='type' />
                             <meta property='og:site_name' content='LogMoney' key='site_name' />
+
+                            <meta name="description" content="Discover LogMoney.app - the smart choice for responsible financial oversight. Discover how students and spenders across the world keep themselves financially accountable."/>
+
+                            <meta property="og:description" content="Discover LogMoney.app - the smart choice for responsible financial oversight. Discover how students and spenders across the world keep themselves financially accountable."/>
+                            <meta property="og:image" content="https://logmoney.app/og.png"/>
+
+
+                            <meta name="twitter:card" content="summary_large_image"/>
+                            <meta property="twitter:domain" content="logmoney.app"/>
+                            <meta property="twitter:url" content="https://logmoney.app"/>
+                            <meta name="twitter:title" content="LogMoney.app | The better money app"/>
+                            <meta name="twitter:description" content="Discover LogMoney.app - the smart choice for responsible financial oversight. Discover how students and spenders across the world keep themselves financially accountable."/>
+                            <meta name="twitter:image" content="https://logmoney.app/og.png"/>
+
+
                           </Head>
                           <Script src="https://simple.logmoney.app/latest.js"  />
                           <noscript>
