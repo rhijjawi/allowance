ee19c8a5540efcecf74842c516bdc781b94aa47c
Ramzi H <github@ramzihijjawi.me>
2024-02-16 00:42:11 +0100

    +

--- utils/functions/discord.tsx
+++ utils/functions/discord.tsx
@@ -1,10 +1,16 @@
-import {Webhook} from "discord-webhook-node"
-export default async function reportFail(pointInProcess : string, error?: string|null) : Promise<void> {
+import axios from "axios";
+
+export default async function reportFail(pointInProcess: string, error?: string | null): Promise<void> {
     const e = error ? error : "";
     const webhookURL = "https://discord.com/api/webhooks/1192491211978707075/XWxe6JIaipke7nC6oa1iZa2JDwilsBRbGnEiXImHpnQJhUPQXfrPNExK_WArAsm_BH0i";
-    const webhook = new Webhook(webhookURL);
-    webhook.setUsername("Report Generation Failed");
-    webhook.setAvatar("https://em-content.zobj.net/source/apple/354/pensive-face_1f614.png");
-    await webhook.send(`Error: ${e} `);
-    return
+    
+    try {
+        await axios.post(webhookURL, {
+            username: "Report Generation Failed",
+            avatar_url: "https://em-content.zobj.net/source/apple/354/pensive-face_1f614.png",
+            content: `Error: ${e}`,
+        });
+    } catch (error) {
+        console.error("Failed to send Discord webhook:", error);
+    }
 }
