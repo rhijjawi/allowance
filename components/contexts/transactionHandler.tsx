369de41d57efc35e35c4f292b762a2ee385d6080
Ramzi H <github@ramzihijjawi.me>
2024-02-21 08:31:24 +0100

    :)

--- components/contexts/transactionHandler.tsx
+++ components/contexts/transactionHandler.tsx
@@ -125,17 +125,17 @@ export function TransactionHandlerProvider({children} : {children: React.ReactNo
                         >
                           Cancel
                         </button>
-                      </div>
-                      <div className="float-right">
-                        <button
-                          type="button"
-                          disabled={loading == true}
-                          className="inline-flex disabled:bg-green-100/60 disabled:cursor-wait justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
-                          onClick={async()=>{await functions["delete"]()}}
-                        >
-                          Confirm
-                        </button>
-                      </div>
+                    </div>
+                    <div className="float-right">
+                      <button
+                        type="button"
+                        disabled={loading == true}
+                        className="inline-flex disabled:bg-green-100/60 disabled:cursor-wait justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                        onClick={async()=>{await functions["delete"]()}}
+                      >
+                        Confirm
+                      </button>
+                    </div>
                     </Dialog.Panel>
                   </Transition.Child>
                 </div>
