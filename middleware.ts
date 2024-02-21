7b466ca0902f9e9b8cfcc2cb5c08dc048957a5b2
Ramzi H <github@ramzihijjawi.me>
2024-02-21 08:35:20 +0100

    bad import

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs/server";
  
 export default authMiddleware({
     publicRoutes: ['/', '/api/(.*)', '/report/(.*)/(.+)', '/trpc(.*)', '/faq', '/debt/calculator', '/legal/privacy-policy', '/legal/terms-of-service', '/.well-known(.*)', '/pricing(.*)'],
