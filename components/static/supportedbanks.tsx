839012bcff34993e4486055231a3b4902213283d
Ramzi H <github@ramzihijjawi.me>
2024-02-27 10:46:27 +0100

    fixed revolut datestring

--- components/static/supportedbanks.tsx
+++ components/static/supportedbanks.tsx
@@ -63,7 +63,7 @@ function Revolut(arr : any){
     else {
     }
     [ , , transactionDate, , label, amount, , currency] = arr
-    const transaction = new Transaction(arr, 4, 5, 7, null, false, 2, false, "yyyy-mm-dd HH:MM:SS", [])
+    const transaction = new Transaction(arr, 4, 5, 7, null, false, 2, false, "yyyy-MM-dd HH:mm:ss", [])
     return transaction.toJSONData()
 }
 
